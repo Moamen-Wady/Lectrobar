c0eb71c7fce1f6d8635bff19661dddd4d9b1a163
Moamen-Wady <moamen.wady@gmail.com>
2025-03-05 06:19:29 +0200

    Final

--- src/RouteChangeHandler.jsx
+++ src/RouteChangeHandler.jsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
-import { useLayoutEffect } from "react";
+// import { useLayoutEffect } from "react";
 import { useLocation } from "react-router";
 
 export default function RouteChangeHandler({ setLoading, hideMenu }) {
   let location = useLocation();
-  useLayoutEffect(() => {
-    function menuHider() {
-      if (window.screen.width <= 768) {
-        hideMenu();
-      }
-    }
-    menuHider();
-  }, [location.pathname]);
+  // useLayoutEffect(() => {
+  //   function menuHider() {}
+  //   menuHider();
+  // }, [location.pathname]);
   useEffect(() => {
     setLoading(true);
+    if (window.screen.width <= 768) {
+      hideMenu();
+    }
     const timer = setTimeout(() => setLoading(false), 400);
     return () => clearTimeout(timer);
   }, [location.pathname]);
